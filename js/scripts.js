5d25471cd2f930c063657fb22361848cef961b7c
Billy Bainbridge <billybainbridge@AT3140-MacBook-Pro.local>
2017-05-09 08:58:26 +0100

    Added all files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// $(window).scroll(function (event) {
+//     var scroll = $(window).scrollTop();
+//     // Do something
+//     console.log(scroll);
+//     if (scroll >= 100) {
+//       $( "#header" ).addClass( "sticky-header" );
+//     }else{
+//       $( "#header" ).removeClass( "sticky-header" );
+//     }
+// });
