62ca87b447ac41f52c473ee25c80ca46a0b79583
Billy Bainbridge <billybainbridge@AT3140-MacBook-Pro.local>
2017-05-10 09:38:37 +0100

    created the navigation menu

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,11 @@
 //       $( "#header" ).removeClass( "sticky-header" );
 //     }
 // });
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
